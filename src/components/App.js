5e836da93b45ba2a9c9e39c6011f3b4a716e8211
Elisabeth Garcia <lisgarciae@gmail.com>
2023-05-24 11:35:04 -0400

    almost

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,9 @@ import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
+//console.log(user);
 
-function App() {
+function App(props) {
   return (
     <div>
       <NavBar />
---
+++ src/components/Links.js
@@ -0,0 +1,17 @@
+import React from "react";
+import user from "../data/user";
+import About from "./About";
+
+function Links(props) {
+    return (
+      <div>
+        <h3>Links</h3>
+        <a href={user.links.github}>{user.links.github}</a>
+        <a href={user.links.linkedin}>{user.links.linkedin}</a>
+        
+      </div>
+    );
+  }
+
+
+export default Links;
